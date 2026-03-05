7edc6f08062ea992f99ed09af9d1a5112c24a71f
Developer-EJ <155644012+Developer-EJ@users.noreply.github.com>
2026-03-05 19:09:22 +0900

    d

--- app.py
+++ app.py
@@ -333,5 +333,5 @@ def likes_comment(post_id, comment_id):
 
 # ============================== Comments ================================
 
-    if __name__ == "__main__":
-	        app.run("0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+	    app.run("0.0.0.0", port=5000, debug=True)
